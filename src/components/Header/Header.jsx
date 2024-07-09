b49a47c6621bea01d5cd42a6f667df6c0215e737
iNa. <aljard@naver.com>
2024-07-09 18:50:47 +0900

    240709_TodoList (styling)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import styles from './Header.module.css';
+import { useDarkMode } from '../../context/DarkModeContext';
+import { HiMoon, HiSun } from 'react-icons/hi';
 
 export default function header({filters, filter, onFilterChange}) {
+  const {darkMode, toggleDarkMode} = useDarkMode();
   return (
-    <header>
-      <ul>
+    <header className={styles.header}>
+      <button onClick={toggleDarkMode} className={styles.toggle}>
+        {!darkMode && <HiMoon />}
+        {darkMode && <HiSun />}
+      </button>
+      <ul className={styles.filters}>
         {filters.map((value, index) => (
           <li key={index}>
-            <button onClick={() => onFilterChange(value)}>{value}</button>
+            <button
+              className={`${styles.filter} ${filter === value && styles.selected}`} 
+              onClick={() => onFilterChange(value)}
+            >
+              {value}
+            </button>
           </li>
         ))}
       </ul>
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,21 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
 import Todo from '../Todo/Todo';
+import styles from './TodoList.module.css';
 
 export default function TodoList({ filter }) {
-  const [todos, setTodos] = useState([
-    {id: '1', text: 'shopping', status: 'active'},
-    {id: '2', text: 'study', status: 'active'},
-  ]);
+  const [todos, setTodos] = useState(readTodosFromLocalStorage);
+
   const handleAdd = (todo) => setTodos([...todos, todo]);
   const handleUpdate = (updated) => 
     setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
   const handleDelete = (deleted) => 
     setTodos(todos.filter((t) => t.id !== deleted.id));
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
   const filtered = getFilteredItems(todos, filter);
   return (
-    <section>
-      <ul>
+    <section className={styles.container}>
+      <ul className={styles.list}>
         {filtered.map((item) => (
           <Todo 
             key={item.id} 
@@ -28,6 +32,11 @@ export default function TodoList({ filter }) {
       <AddTodo onAdd={handleAdd}/>
     </section>
   );
+}
+
+function readTodosFromLocalStorage() {
+  const todos = localStorage.getItem('todos');
+  return todos ? JSON.parse(todos) : [];
 }
 
 function getFilteredItems(todos, filter) {
